57ca2b2378a7201d041f79b3d78818f9874ccbd1
dilandiaz67 <xochiquixtle.diaz.dilan.m532@gmail.com>
2026-02-24 14:41:15 -0600

    Actualizar 01_create_collections.js

--- scripts/01_create_collections.js
+++ scripts/01_create_collections.js
@@ -1,1 +1,48 @@
-Código 
+Código
+ {
+  "nombre_lista_reproduccion": "Leyendas Oscuras de México",
+  "descripcion": "Un recorrido sonoro por las leyendas más aterradoras y misteriosas de México.",
+  "creador": {
+    "integrantes": [
+      {
+        "nombre": "Dilan Guillermo",
+        "correo_electronico": "dilan.guillermo@escuela.edu"
+      },
+      {
+        "nombre": "Klara Karime",
+        "correo_electronico": "klara.karime@escuela.edu"
+      },
+      {
+        "nombre": "Ivan Mauricio",
+        "correo_electronico": "ivan.mauricio@escuela.edu"
+      },
+      {
+        "nombre": "Isabel Baigen",
+        "correo_electronico": "isabel.baigen@escuela.edu"
+      }
+    ],
+    "fecha_creacion": "2025-02-20"
+  },
+  "categorias": ["Terror"],
+  "seguidores": 1200,
+  "episodios": [
+    {
+      "titulo": "La Llorona",
+      "duracion_minutos": 35,
+      "descripcion": "La historia de la mujer que vaga por las noches buscando a sus hijos.",
+      "region": "Ciudad de México"
+    },
+    {
+      "titulo": "El Charro Negro",
+      "duracion_minutos": 40,
+      "descripcion": "Un misterioso jinete que aparece en caminos solitarios ofreciendo riquezas.",
+      "region": "Jalisco"
+    },
+    {
+      "titulo": "La Isla de las Muñecas",
+      "duracion_minutos": 30,
+      "descripcion": "El inquietante relato de una isla llena de muñecas colgadas en los árboles.",
+      "region": "Xochimilco"
+    }
+  ]
+}
